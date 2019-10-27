9982a9fabce8632da7dee79dd00e54aa7cde5323
Alex Luong <alex@alexluong.com>
2019-10-27 01:20:29 -0500

    layout inside app

---
+++ src/components/Navigation.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { AuthContext } from "./Auth";
+import { Box, Flex, Button, Heading } from "@chakra-ui/core";
+import { COLORS } from "../utils/constants";
+
+function Navigation({ history }) {
+  const { signOut } = React.useContext(AuthContext);
+
+  function signOutAndNavigate() {
+    signOut().then(() => history.push("/"));
+  }
+
+  return (
+    <Box px={[8, 12, 16, 24]} py={6}>
+      <Flex
+        align={["start", "start", "center"]}
+        justify="space-between"
+        direction={["column", "column", "row"]}
+        w="100%"
+        maxW={1200}
+        mx="auto"
+      >
+        <Link to="/">
+          <Heading as="h1" size="md" display="block" mb={[4, 4, 0]}>
+            RideShare
+          </Heading>
+        </Link>
+        <Flex
+          align="center"
+          justify="start"
+          direction={["column-reverse", "column-reverse", "row"]}
+        >
+          <Button
+            as={Link}
+            to="/ride/new"
+            variant="outline"
+            variantColor={COLORS.primary}
+            display={["none", "none", "inline-flex"]}
+            mr={2}
+          >
+            Request Ride
+          </Button>
+          <Button
+            as={Link}
+            to="/"
+            variant="ghost"
+            variantColor={COLORS.primary}
+            display={["none", "none", "inline-flex"]}
+            mr={2}
+          >
+            Home
+          </Button>
+          <Button
+            display={["none", "none", "inline-flex"]}
+            mr={2}
+            onClick={signOutAndNavigate}
+          >
+            Sign Out
+          </Button>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
+
+export default withRouter(Navigation);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,12 @@
 import React from "react";
-import firebase from "firebase/app";
-import { Link } from "react-router-dom";
-import { Button, Box } from "@chakra-ui/core";
+import { Heading } from "@chakra-ui/core";
+import Layout from "../components/Layout";
 
 function Home() {
   return (
-    <Box p={5}>
-      <h1>Home</h1>
-      <Box>
-        <Button as={Link} to="/ride/new" variantColor="blue">
-          Request a Ride
-        </Button>
-      </Box>
-      <Box>
-        <button onClick={() => firebase.auth().signOut()}>Sign out</button>
-      </Box>
-    </Box>
+    <Layout>
+      <Heading as="h1">Home</Heading>
+    </Layout>
   );
 }
 
